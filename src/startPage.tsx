596460455977e99c5c62e67e36967d58a2774644
aungbbo <aungbbo48@gmail.com>
2025-12-23 19:31:33 -0800

    update readme

--- src/startPage.tsx
+++ src/startPage.tsx
@@ -1,6 +1,15 @@
-import mindorbitLogo from "./assets/mindorbit.png";
+import mindorbitLogo from "./assets/mindorbit_logo.png";
 import "./App.css";
 import { Link } from "react-router-dom";
+// import {
+//   Reality,
+//   SceneGraph,
+//   BoxEntity,
+//   ModelEntity,
+//   UnlitMaterial,
+//   ModelAsset,
+//   Model,
+// } from "@webspatial/react-sdk";
 
 function StartPage() {
   return (
@@ -10,6 +19,11 @@ function StartPage() {
         alt="MindOrbit Logo"
         className="mindorbit-logo"
       />
+      {/* <Model style={{ width: "200px", height: "200px" }}>
+        <source src="./assets/mindorbit_3d_logo.usdz" type="model/vnd.usdz+zip" />
+        <div>Fallback text if model fails to load</div>
+      </Model> */}
+
       <Link to="/notes" className="enter-button">
         Enter Notegarden
       </Link>
